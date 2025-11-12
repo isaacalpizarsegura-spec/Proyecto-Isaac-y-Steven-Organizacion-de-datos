633b0e192de1d5073ef54bb60b360284955bacd5
StevenCa2007 <stevenca746@gmail.com>
2025-11-11 18:40:55 -0600

    primer avance

--- Proyecto C++/Proyecto C++.cpp
+++ Proyecto C++/Proyecto C++.cpp
@@ -1,7 +1,31 @@
 // Proyecto C++.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
-
 #include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <limits>
+
+
+using namespace std;
+
+
+class Contacto {
+public:
+    string nombre;
+    string telefono;
+    string correo;
+
+    Contacto() {}
+    Contacto(string n, string t, string c) {
+        nombre = n;
+        telefono = t;
+        correo = c;
+    }
+};
+
 
 int main()
 {
